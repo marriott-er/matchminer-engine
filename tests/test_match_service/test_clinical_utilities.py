d670261baf0b5ccc46935d0c45d67ddc74c0d8ea
Zachary <zwiesler@jimmy.harvard.edu>
2018-11-05 15:43:05 -0500

    reorganized clinical utilities and all unit tests pass

--- tests/test_match_service/test_clinical_utilities.py
+++ tests/test_match_service/test_clinical_utilities.py
@@ -1,11 +1,9 @@
-import unittest
-
+from tests.test_match_service import TestQueryUtilitiesShared
 from src.services.match_service.query_utilities.clinical_utilities import ClinicalUtilities
 from src.utilities.settings import oncotree_all_solid_text, oncotree_all_liquid_text
-from tests.test_match_service import all_nsclc_cancer_types, all_liquid_cancer_types, all_solid_cancer_types
 
 
-class TestClinicalUtilities(unittest.TestCase):
+class TestClinicalUtilities(TestQueryUtilitiesShared):
 
     def setUp(self):
         super(TestClinicalUtilities, self).setUp()
@@ -23,11 +21,11 @@ class TestClinicalUtilities(unittest.TestCase):
     def test_expand_oncotree_diagnosis(self):
 
         diagnoses = self.c.expand_oncotree_diagnosis(diagnosis='Non-Small Cell Lung Cancer')
-        assert sorted(diagnoses) == sorted(all_nsclc_cancer_types), sorted(diagnoses)
+        assert sorted(diagnoses) == sorted(self.all_nsclc_cancer_types), sorted(diagnoses)
 
     def test_expand_grouped_diagnoses(self):
 
         all_liquid_cancers = self.c.expand_grouped_diagnoses(diagnosis=oncotree_all_liquid_text)
         all_solid_cancers = self.c.expand_grouped_diagnoses(diagnosis=oncotree_all_solid_text)
-        assert sorted(all_liquid_cancers) == all_liquid_cancer_types, sorted(all_liquid_cancers)
-        assert sorted(all_solid_cancers) == all_solid_cancer_types, sorted(all_solid_cancers)
+        assert sorted(all_liquid_cancers) == self.all_liquid_cancer_types, sorted(all_liquid_cancers)
+        assert sorted(all_solid_cancers) == self.all_solid_cancer_types, sorted(all_solid_cancers)
