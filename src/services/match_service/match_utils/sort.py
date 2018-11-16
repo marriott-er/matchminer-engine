9779bc28686c22ffcc3592419f08ea80dd856cc2
Zachary <zwiesler@jimmy.harvard.edu>
2018-11-16 16:29:45 -0500

    sort by tier passes unit test

--- src/services/match_service/match_utils/sort.py
+++ src/services/match_service/match_utils/sort.py
@@ -1,9 +1,41 @@
 """Copyright 2016 Dana-Farber Cancer Institute"""
 
-import pandas as pd
 import logging
+import pandas as pd
+
+from src.data_store import key_names as kn
 
 logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(message)s', )
+
+
+class Sort:
+
+    def __init__(self, trial_matches):
+        self.trial_matches = trial_matches
+
+        self.trial_matches_df = None
+        self.all_sample_ids = None
+        self.f_alive = None
+        self.f_open = None
+        self.f_no_svs = None
+
+    def prep_trial_matches(self):
+        """
+        Add columns to dataframe to facilitate sorting
+
+        :return: {null}
+        """
+        self.trial_matches_df = pd.DataFrame.from_dict(self.trial_matches)
+
+        self.trial_matches_df['has_mut'] = self.trial_matches_df[kn.mutation_list_col].apply(self._has_vc)
+        self.trial_matches_df['has_cnv'] = self.trial_matches_df[kn.cnv_list_col].apply(self._has_vc)
+        self.trial_matches_df['has_sv'] = self.trial_matches_df[kn.sv_list_col].apply(self._has_vc)
+        self.trial_matches_df['has_wt'] = self.trial_matches_df[kn.wt_genes_col].apply(self._has_vc)
+
+        self.all_sample_ids = self.trial_matches_df[kn.sample_id_col].unique().tolist()
+        self.f_alive = (self.trial_matches_df[kn.vital_status_col] == 'alive')
+        self.f_open = (self.trial_matches_df[kn.tm_trial_accrual_status_col] == 'open')
+        self.f_no_svs = (self.trial_matches_df['has_sv'] == False)
 
 
 def add_sort_order(trial_matches):
@@ -16,25 +48,19 @@ def add_sort_order(trial_matches):
     (5) Then sort by reverse protocol number (high > low)
 
     :param trial_matches: List of trial match dictionaries
-    :return: List of trial match dictionaries with two additional columns:
-        (1) sort_order: Order in which to display the matches
-        (2) freq: Frequency with which this trial match appears throughout the entire patient cohort
+    :return: List of trial match dictionaries with sort_order column filled in:
     """
     # todo unit test
-    trial_match_df = pd.DataFrame.from_dict(trial_matches)
+    if len(trial_matches) == 0:
+        return trial_matches
 
-    if len(trial_match_df.index) == 0:
-        return trial_match_df
+    s = Sort(trial_matches=trial_matches)
+    s.prep_trial_matches()
 
-    f1 = (trial_match_df['vital_status'] == 'alive')
-    f2 = (trial_match_df['trial_accrual_status'] == 'open')
-    f3 = (trial_match_df['genomic_alteration'].str.strip().str.title() != 'Structural Variation')
-    all_sample_ids = trial_match_df.sample_id.unique().tolist()
     master_sort_order = {}
-
-    for sample_id in all_sample_ids:
-        f4 = (trial_match_df['sample_id'] == sample_id)
-        df = trial_match_df[f1 & f2 & f3 & f4]
+    for sample_id in s.all_sample_ids:
+        f_sample_id = (s.trial_matches_df[kn.sample_id_col] == sample_id)
+        df = s.trial_matches_df[s.f_alive & s.f_open & s.f_no_svs & f_sample_id]
         matches = df.T.to_dict().values()
 
         # The sort order dictionary keeps track of the priority for each sort category for each match
@@ -47,7 +73,7 @@ def add_sort_order(trial_matches):
 
         for match in matches:
 
-            idx = (match['sample_id'], match['protocol_no'])
+            idx = (match[kn.sample_id_col], match[kn.tm_trial_protocol_no_col])
             if idx not in sort_order:
                 sort_order[idx] = []
 
@@ -57,66 +83,14 @@ def add_sort_order(trial_matches):
             sort_order = sort_by_coordinating_center(match, sort_order)
 
         sort_order = sort_by_reverse_protocol_no(matches, sort_order)
-
-        # for k, v in sort_order.iteritems():
-        #     print '%s | %s' % (k, v)
-
         master_sort_order = final_sort(sort_order, master_sort_order)
 
-    trial_match_df['sort_order'] = trial_match_df.apply(lambda x: master_sort_order[(x['sample_id'], x['protocol_no'])]
-                                                        if (x['sample_id'], x['protocol_no']) in master_sort_order
-                                                        else -1, axis=1)
-    return trial_match_df
+    s.trial_matches_df['sort_order'] = s.trial_matches_df.apply(
+        lambda x: master_sort_order[(x[kn.sample_id_col], x[kn.tm_trial_protocol_no_col])]
+        if (x[kn.sample_id_col], x[kn.tm_trial_protocol_no_col]) in master_sort_order
+        else -1, axis=1)
 
-
-def sort_by_tier(match, sort_order):
-    """
-    Highest priority sorting
-    """
-    # todo unit test
-    idx = (match['sample_id'], match['protocol_no'])
-
-    if 'mmr_status' in match and pd.notnull(match['mmr_status']):
-        sort_order[idx] = add_sort_value(sort_value=0,
-                                         priority=0,
-                                         sort_order_li=sort_order[idx])
-
-    elif 'tier' in match and match['tier'] == 1:
-        sort_order[idx] = add_sort_value(sort_value=1,
-                                         priority=0,
-                                         sort_order_li=sort_order[idx])
-
-    elif 'tier' in match and match['tier'] == 2:
-        sort_order[idx] = add_sort_value(sort_value=2,
-                                         priority=0,
-                                         sort_order_li=sort_order[idx])
-
-    elif 'variant_category' in match and match['variant_category'] == 'CNV':
-        sort_order[idx] = add_sort_value(sort_value=3,
-                                         priority=0,
-                                         sort_order_li=sort_order[idx])
-
-    elif 'tier' in match and match['tier'] == 3:
-        sort_order[idx] = add_sort_value(sort_value=4,
-                                         priority=0,
-                                         sort_order_li=sort_order[idx])
-
-    elif 'tier' in match and match['tier'] == 4:
-        sort_order[idx] = add_sort_value(sort_value=5,
-                                         priority=0,
-                                         sort_order_li=sort_order[idx])
-
-    elif 'wildtype' in match and match['wildtype'] is True:
-        sort_order[idx] = add_sort_value(sort_value=6,
-                                         priority=0,
-                                         sort_order_li=sort_order[idx])
-
-    else:
-        sort_order[idx] = add_sort_value(sort_value=7,
-                                         priority=0,
-                                         sort_order_li=sort_order[idx])
-
-    return sort_order
+    return s.trial_matches_df.to_json(orient='records', date_format='iso')
 
 
 def sort_by_match_type(match, sort_order):
@@ -124,7 +98,7 @@ def sort_by_match_type(match, sort_order):
     Second highest priority sorting
     """
     # todo unit test
-    idx = (match['sample_id'], match['protocol_no'])
+    idx = (match[kn.sample_id_col], match[kn.tm_trial_protocol_no_col])
 
     if 'match_type' in match and match['match_type'] == 'variant':
         sort_order[idx] = add_sort_value(sort_value=0,
@@ -148,7 +122,7 @@ def sort_by_cancer_type(match, sort_order):
     Third highest priority sorting
     """
 
-    idx = (match['sample_id'], match['protocol_no'])
+    idx = (match[kn.sample_id_col], match[kn.tm_trial_protocol_no_col])
 
     if 'cancer_type_match' in match and match['cancer_type_match'] == 'specific':
         sort_order[idx] = add_sort_value(sort_value=0,
@@ -178,7 +152,7 @@ def sort_by_coordinating_center(match, sort_order):
     Fourth highest priority sorting
     """
     # todo unit test
-    idx = (match['sample_id'], match['protocol_no'])
+    idx = (match[kn.sample_id_col], match[kn.tm_trial_protocol_no_col])
 
     if 'coordinating_center' in match and match['coordinating_center'] == 'Dana-Farber Cancer Institute':
         sort_order[idx] = add_sort_value(sort_value=0,
@@ -197,13 +171,13 @@ def sort_by_reverse_protocol_no(matches, sort_order):
     Lowest priority sorting
     """
     # todo unit test
-    rev_prot_no_sort = sorted(matches, key=lambda k: int(k['protocol_no'].split('-')[0]))
+    rev_prot_no_sort = sorted(matches, key=lambda k: int(k[kn.tm_trial_protocol_no_col].split('-')[0]))
     i = 0
 
     for match in rev_prot_no_sort[::-1]:
 
-        if len(sort_order[(match['sample_id'], match['protocol_no'])]) == 4:
-            sort_order[(match['sample_id'], match['protocol_no'])].append(i)
+        if len(sort_order[(match[kn.sample_id_col], match[kn.tm_trial_protocol_no_col])]) == 4:
+            sort_order[(match[kn.sample_id_col], match[kn.tm_trial_protocol_no_col])].append(i)
             i += 1
 
     return sort_order
@@ -223,6 +197,19 @@ def final_sort(sort_order, master_sort_order):
     return master_sort_order
 
 
+# --------- #
+# Utilities #
+# --------- #
+def has_vc(vc):
+    """
+    Assess if row has variants in the given variant category
+
+    :param vc: {list or null}
+    :return: {bool or null}
+    """
+    return len(vc) > 0 if isinstance(vc, list) else vc
+
+
 def add_sort_value(sort_value, priority, sort_order_li):
     """
     Adds the sort value, independent of the logic required to assess and determine that value.
@@ -233,7 +220,6 @@ def add_sort_value(sort_value, priority, sort_order_li):
         (e.g. tier, match_type, etc.)
     :param sort_order_li: The match-specific sort order list so far
     """
-    # todo unit test
     if len(sort_order_li) >= priority + 1:
 
         if sort_value < sort_order_li[priority]:
@@ -242,3 +228,74 @@ def add_sort_value(sort_value, priority, sort_order_li):
         sort_order_li.append(sort_value)
 
     return sort_order_li
+
+
+def extract_tier(match):
+    """
+    Extract tier from mutation list
+
+    :param match: {dict}
+    :return: {int or null}
+    """
+    if kn.mutation_list_col not in match or len(match[kn.mutation_list_col]) == 0:
+        return None
+
+    tiers = [i[kn.tier_col] for i in match[kn.mutation_list_col]
+             if kn.tier_col in i and i[kn.tier_col] is not None]
+    return min(tiers) if len(tiers) > 0 else None
+
+
+# --------------- #
+# Sorting methods #
+# --------------- #
+def sort_by_tier(match, sort_order):
+    """
+    Highest priority sorting
+    Signatures > Tier 1 mutations > Tier 2 mutations > CNVs > Tier 3 mutations > Tier 4 mutations > Wild-type
+
+    :return {dict of lists}
+    """
+    idx = (match[kn.sample_id_col], match[kn.tm_trial_protocol_no_col])
+    lowest_tier = extract_tier(match)
+
+    if kn.mmr_status_col in match and pd.notnull(match[kn.mmr_status_col]):
+        sort_order[idx] = add_sort_value(sort_value=0,
+                                         priority=0,
+                                         sort_order_li=sort_order[idx])
+
+    elif lowest_tier == 1:
+        sort_order[idx] = add_sort_value(sort_value=1,
+                                         priority=0,
+                                         sort_order_li=sort_order[idx])
+
+    elif lowest_tier == 2:
+        sort_order[idx] = add_sort_value(sort_value=2,
+                                         priority=0,
+                                         sort_order_li=sort_order[idx])
+
+    elif kn.cnv_list_col in match and len(match[kn.cnv_list_col]) > 0:
+        sort_order[idx] = add_sort_value(sort_value=3,
+                                         priority=0,
+                                         sort_order_li=sort_order[idx])
+
+    elif lowest_tier == 3:
+        sort_order[idx] = add_sort_value(sort_value=4,
+                                         priority=0,
+                                         sort_order_li=sort_order[idx])
+
+    elif lowest_tier == 4:
+        sort_order[idx] = add_sort_value(sort_value=5,
+                                         priority=0,
+                                         sort_order_li=sort_order[idx])
+
+    elif kn.wt_genes_col in match and len(match[kn.wt_genes_col]):
+        sort_order[idx] = add_sort_value(sort_value=6,
+                                         priority=0,
+                                         sort_order_li=sort_order[idx])
+
+    else:
+        sort_order[idx] = add_sort_value(sort_value=7,
+                                         priority=0,
+                                         sort_order_li=sort_order[idx])
+
+    return sort_order
---
+++ tests/test_match_service/test_match_utils/test_matchengine/test_sort.py
@@ -0,0 +1,347 @@
+"""Copyright 2016 Dana-Farber Cancer Institute"""
+
+import pandas as pd
+
+from src.utilities import settings as s
+from src.data_store import key_names as kn
+from tests.test_match_service import TestQueryUtilitiesShared
+from src.services.match_service.match_utils.sort import *
+
+
+class TestSort(TestQueryUtilitiesShared):
+
+    def setUp(self):
+        super(TestSort, self).setUp()
+
+        self.s = Sort(trial_matches=[])
+
+    def tearDown(self):
+        pass
+
+    def test_has_vc(self):
+
+        assert has_vc(None) is None
+        assert has_vc([]) is False
+        assert has_vc([[1]]) is True
+
+    def test_prep_trial_matches(self):
+
+        data = [
+            {
+                kn.sample_id_col: 'DEV-01',
+                kn.vital_status_col: 'alive',
+                kn.tm_trial_accrual_status_col: 'open',
+                kn.mutation_list_col: [{kn.hugo_symbol_col: 'BRAF'}],
+                kn.cnv_list_col: [],
+                kn.sv_list_col: [],
+                kn.wt_genes_col: None
+            },
+            {
+                kn.sample_id_col: 'DEV-02',
+                kn.vital_status_col: 'alive',
+                kn.tm_trial_accrual_status_col: 'open',
+                kn.mutation_list_col: [],
+                kn.cnv_list_col: [{kn.hugo_symbol_col: 'BRAF'}],
+                kn.sv_list_col: [],
+                kn.wt_genes_col: None
+            },
+            {
+                kn.sample_id_col: 'DEV-03',
+                kn.vital_status_col: 'alive',
+                kn.tm_trial_accrual_status_col: 'open',
+                kn.mutation_list_col: [],
+                kn.cnv_list_col: [],
+                kn.sv_list_col: [{kn.hugo_symbol_col: 'BRAF'}],
+                kn.wt_genes_col: None
+            },
+            {
+                kn.sample_id_col: 'DEV-03',
+                kn.mutation_list_col: [],
+                kn.cnv_list_col: [],
+                kn.sv_list_col: None,
+                kn.wt_genes_col: [{kn.hugo_symbol_col: 'BRAF'}]
+            }
+        ]
+        s = Sort(trial_matches=data)
+        s.prep_trial_matches()
+        assert s.trial_matches_df['has_mut'].tolist() == [True, False, False, False]
+        assert s.trial_matches_df['has_cnv'].tolist() == [False, True, False, False]
+        assert s.trial_matches_df['has_sv'].tolist() == [False, False, True, None]
+        assert s.trial_matches_df['has_wt'].tolist() == [None, None, None, True]
+        assert sorted(s.all_sample_ids) == ['DEV-01', 'DEV-02', 'DEV-03']
+        assert s.f_alive is not None
+        assert s.f_open is not None
+        assert s.f_no_svs is not None
+
+    def test_add_sort_value(self):
+
+        li = []
+        li = add_sort_value(8, 0, li)
+        assert li == [8]
+
+        li = add_sort_value(0, 0, li)
+        assert li == [0]
+
+        li = add_sort_value(1, 1, li)
+        assert li == [0, 1]
+
+        li = [0, 1, 0, 0]
+        li = add_sort_value(1, 0, li)
+        assert li == [0, 1, 0, 0]
+
+    def test_extract_tier(self):
+
+        m1 = {}
+        m2 = {kn.mutation_list_col: []}
+        m3 = {kn.mutation_list_col: [{}]}
+        m4 = {kn.mutation_list_col: [{kn.tier_col: 1}]}
+        m5 = {kn.mutation_list_col: [{kn.tier_col: 1}, {}]}
+        m6 = {kn.mutation_list_col: [{kn.tier_col: 1}, {kn.tier_col: None}]}
+        m7 = {kn.mutation_list_col: [{kn.tier_col: 1}, {kn.tier_col: 2}]}
+        assert extract_tier(m1) is None
+        assert extract_tier(m2) is None
+        assert extract_tier(m3) is None
+        assert extract_tier(m4) == 1
+        assert extract_tier(m5) == 1
+        assert extract_tier(m6) == 1
+        assert extract_tier(m7) == 1
+
+    def test_sort_by_tier(self):
+
+        # MMR Deficient
+        m1 = ('DEV-01', '00-001')
+        sort_order = {m1: []}
+        m1_data = {
+            kn.mrn_col: 'MRN-01',
+            kn.sample_id_col: m1[0],
+            kn.tm_trial_protocol_no_col: m1[1],
+            kn.mmr_status_col: s.mmr_status_deficient_val
+        }
+        sort_order = sort_by_tier(m1_data, sort_order)
+        assert sort_order[m1][0] == 0
+
+        # Tier 1 BRAF p.V600E
+        sort_order = {m1: []}
+        m2_data = {
+            kn.mrn_col: 'MRN-01',
+            kn.sample_id_col: m1[0],
+            kn.tm_trial_protocol_no_col: m1[1],
+            kn.wt_genes_col: [{kn.hugo_symbol_col: 'BRAF'}],
+            kn.cnv_list_col: [{kn.hugo_symbol_col: 'BRAF'}],
+            kn.mutation_list_col: [
+                {kn.hugo_symbol_col: 'BRAF', kn.protein_change_col: 'p.V600E', kn.tier_col: 1},
+                {kn.hugo_symbol_col: 'BRAF', kn.protein_change_col: 'p.G469A', kn.tier_col: 2}
+            ]
+        }
+        sort_order = sort_by_tier(m2_data, sort_order)
+        assert sort_order[m1][0] == 1
+
+        # Tier 2 BRAF p.G469A
+        sort_order = {m1: []}
+        m3_data = {
+            kn.mrn_col: 'MRN-01',
+            kn.sample_id_col: m1[0],
+            kn.tm_trial_protocol_no_col: m1[1],
+            kn.wt_genes_col: [{kn.hugo_symbol_col: 'BRAF'}],
+            kn.cnv_list_col: [{kn.hugo_symbol_col: 'BRAF'}],
+            kn.mutation_list_col: [
+                {kn.hugo_symbol_col: 'BRAF', kn.protein_change_col: 'p.G469A', kn.tier_col: 2}
+            ]
+        }
+        sort_order = sort_by_tier(m3_data, sort_order)
+        assert sort_order[m1][0] == 2
+
+        # Tier 3 BRAF p.N581S
+        m4_data = {
+            kn.mrn_col: 'MRN-01',
+            kn.sample_id_col: m1[0],
+            kn.tm_trial_protocol_no_col: m1[1],
+            kn.wt_genes_col: [{kn.hugo_symbol_col: 'BRAF'}],
+            kn.mutation_list_col: [
+                {kn.hugo_symbol_col: 'BRAF', kn.protein_change_col: 'p.N581S', kn.tier_col: 3}
+            ]
+        }
+        sort_order = {m1: []}
+        sort_order = sort_by_tier(m4_data, sort_order)
+        assert sort_order[m1][0] == 4
+
+        # Tier 4 BRAF p.E695Q
+        sort_order = {m1: []}
+        m5_data = {
+            kn.mrn_col: 'MRN-01',
+            kn.sample_id_col: m1[0],
+            kn.tm_trial_protocol_no_col: m1[1],
+            kn.wt_genes_col: [{kn.hugo_symbol_col: 'BRAF'}],
+            kn.mutation_list_col: [
+                {kn.hugo_symbol_col: 'BRAF', kn.protein_change_col: 'p.E695Q', kn.tier_col: 4}
+            ]
+        }
+        sort_order = sort_by_tier(m5_data, sort_order)
+        assert sort_order[m1][0] == 5
+
+        # Weird unknown edge case
+        sort_order = {m1: []}
+        m6_data = {
+            kn.mrn_col: 'MRN-01',
+            kn.sample_id_col: m1[0],
+            kn.tm_trial_protocol_no_col: m1[1],
+        }
+        sort_order = sort_by_tier(m6_data, sort_order)
+        assert sort_order[m1][0] == 7
+
+        # BRAF CNV
+        sort_order = {m1: []}
+        m7_data = {
+            kn.mrn_col: 'MRN-01',
+            kn.sample_id_col: m1[0],
+            kn.tm_trial_protocol_no_col: m1[1],
+            kn.cnv_list_col: [{kn.hugo_symbol_col: 'BRAF'}],
+            kn.wt_genes_col: [{kn.hugo_symbol_col: 'BRAF'}],
+            kn.mutation_list_col: [
+                {kn.hugo_symbol_col: 'BRAF', kn.protein_change_col: 'p.E695Q', kn.tier_col: 4}
+            ]
+        }
+        sort_order = sort_by_tier(m7_data, sort_order)
+        assert sort_order[m1][0] == 3
+
+        # Wild-type BRAF
+        sort_order = {m1: []}
+        m8_data = {
+            kn.mrn_col: 'MRN-01',
+            kn.sample_id_col: m1[0],
+            kn.tm_trial_protocol_no_col: m1[1],
+            kn.wt_genes_col: [{kn.hugo_symbol_col: 'BRAF'}]
+        }
+        sort_order = sort_by_tier(m8_data, sort_order)
+        assert sort_order[m1][0] == 6
+
+    def test_sort_by_match_type(self):
+
+        sort_order = {('01', 'p01'): [0]}
+        match = {
+            kn.mrn_col: '01',
+            kn.sample_id_col: '01',
+            kn.tm_trial_protocol_no_col: 'p01',
+            'match_type': None
+        }
+        sort_order = sort_by_match_type(match, sort_order)
+        assert sort_order[('01', 'p01')][1] == 2
+
+        match['match_type'] = 'gene'
+        sort_order = sort_by_match_type(match, sort_order)
+        assert sort_order[('01', 'p01')][1] == 1
+
+        match['match_type'] = 'variant'
+        sort_order = sort_by_match_type(match, sort_order)
+        assert sort_order[('01', 'p01')][1] == 0
+
+    def test_sort_by_cancer_type(self):
+
+        sort_order = {('01', 'p01'): [0, 0]}
+        match = {
+            kn.mrn_col: '01',
+            kn.sample_id_col: '01',
+            kn.tm_trial_protocol_no_col: 'p01',
+            'cancer_type_match': 'all_solid'
+        }
+        sort_order = sort_by_cancer_type(match, sort_order)
+        assert sort_order[('01', 'p01')][2] == 1
+
+        match['cancer_type_match'] = 'all_liquid'
+        sort_order = sort_by_cancer_type(match, sort_order)
+        assert sort_order[('01', 'p01')][2] == 1
+
+        match['cancer_type_match'] = 'specific'
+        sort_order = sort_by_cancer_type(match, sort_order)
+        assert sort_order[('01', 'p01')][2] == 0
+
+        match['cancer_type_match'] = 'unknown'
+        sort_order = sort_by_cancer_type(match, sort_order)
+        assert sort_order[('01', 'p01')][2] == 0
+
+    def test_sort_by_coordinating_center(self):
+
+        sort_order = {('01', 'p01'): [0, 0, 0]}
+        match = {
+            kn.mrn_col: '01',
+            kn.sample_id_col: '01',
+            kn.tm_trial_protocol_no_col: 'p01',
+            'coordinating_center': 'Massachussetts General Hospital'
+        }
+        sort_order = sort_by_coordinating_center(match, sort_order)
+        assert sort_order[('01', 'p01')][3] == 1
+
+        match['coordinating_center'] = 'Dana-Farber Cancer Institute'
+        sort_order = sort_by_coordinating_center(match, sort_order)
+        assert sort_order[('01', 'p01')][3] == 0
+
+    def test_sort_by_reverse_protocol_no(self):
+
+        sort_order = {
+            ('01', '11-111'): [0, 0, 0, 0],
+            ('01', '09-999'): [0, 0, 0, 1],
+            ('01', '15-000'): [7, 0, 0, 0],
+            ('01', '15-111'): [7, 0, 0, 0],
+            ('01', '22-222'): [7, 1, 0, 0],
+        }
+        matches = [
+            {kn.tm_trial_protocol_no_col: '11-111', kn.sample_id_col: '01'},
+            {kn.tm_trial_protocol_no_col: '09-999', kn.sample_id_col: '01'},
+            {kn.tm_trial_protocol_no_col: '15-000', kn.sample_id_col: '01'},
+            {kn.tm_trial_protocol_no_col: '15-111', kn.sample_id_col: '01'},
+            {kn.tm_trial_protocol_no_col: '22-222', kn.sample_id_col: '01'},
+            {kn.tm_trial_protocol_no_col: '22-222', kn.sample_id_col: '01'}
+        ]
+        sort_order = sort_by_reverse_protocol_no(matches, sort_order)
+        assert sort_order[('01', '11-111')] == [0, 0, 0, 0, 3]
+        assert sort_order[('01', '09-999')] == [0, 0, 0, 1, 4]
+        assert sort_order[('01', '15-000')] == [7, 0, 0, 0, 2]
+        assert sort_order[('01', '15-111')] == [7, 0, 0, 0, 1]
+        assert sort_order[('01', '15-111')] == [7, 0, 0, 0, 1]
+        assert sort_order[('01', '15-111')] == [7, 0, 0, 0, 1]
+        assert sort_order[('01', '22-222')] == [7, 1, 0, 0, 0]
+
+    def test_final_sort(self):
+
+        mso = {}
+        sort_order = {
+            ('01', '11-111'): [0, 0, 0, 0, 1],
+            ('01', '09-999'): [0, 0, 0, 1, 0],
+            ('01', '12-000'): [2, 3, 1, 0, 2],
+            ('01', '12-222'): [2, 3, 1, 1, 3],
+            ('01', '12-333'): [2, 3, 1, 0, 4],
+            ('01', '13-000'): [2, 0, 0, 0, 5],
+            ('01', '15-333'): [2, 3, 1, 0, 6]
+        }
+        mso = final_sort(sort_order, mso)
+        assert mso[('01', '11-111')] == 0
+        assert mso[('01', '09-999')] == 1
+        assert mso[('01', '13-000')] == 2
+        assert mso[('01', '12-000')] == 3
+        assert mso[('01', '12-333')] == 4
+        assert mso[('01', '15-333')] == 5
+        assert mso[('01', '12-222')] == 6
+
+    def test_add_sort_order(self):
+
+        tm = self.get_demo_trial_matches()
+        tm = add_sort_order(tm)
+
+        # print tm[[kn.tm_trial_protocol_no_col, 'sort_order']].sort_values(by='sort_order', ascending=True)
+        assert tm[[kn.tm_trial_protocol_no_col, 'sort_order']].sort_values(by='sort_order', ascending=True).protocol_no.tolist() == \
+            [
+                '0003-000',  # tm13 (SV match (gets a sort order of -1))
+                '0001-000',  # tm11 (mmr status deficient)
+                '111-000',  # tm1  (tier 1, variant match, specific cancer type, DFCI, higher protocol #)
+                '000-000',   # tm10 (tier 1, variant match, specific cancer type, DFCI, lower protocol #)
+                '999-000',   # tm9  (tier 1, variant match, specific cancer type, MGH)
+                '888-000',   # tm8  (tier 1, variant match, solid cancer type)
+                '777-000',   # tm7  (tier 1, gene match)
+                '444-000',   # tm4  (tier 2)
+                '333-000',   # tm3  (CNV)
+                '555-000',   # tm5  (tier 3)
+                '666-000',   # tm6  (tier 4, higher protocol #)
+                '222-000',   # tm2  (tier 4)
+                '0002-000',  # tm12 (wildtype)
+                '0004-000',  # tm14 (clinical only)
+            ]
