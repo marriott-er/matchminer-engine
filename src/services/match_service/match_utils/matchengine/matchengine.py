ab075b69081ee054f0657de6e82d33d0c8f8a2df
Zachary <zwiesler@jimmy.harvard.edu>
2018-12-07 14:37:15 -0500

    fixed issue with parsing structural variant nodes

--- src/services/match_service/match_utils/matchengine/matchengine.py
+++ src/services/match_service/match_utils/matchengine/matchengine.py
@@ -141,6 +141,7 @@ class MatchEngine(AssessNodeUtils, IntersectResultsUtils):
             sample[kn.mr_trial_dose_code_col] = self.trial_info['dose_code'] if 'dose_code' in self.trial_info else None
             sample[kn.mr_coordinating_center_col] = self.trial_info[s.trial_coordinating_center_col]
 
+            print sample
             if not self.validator.validate_document(sample):
                 raise ValueError('%s sample did not pass data validation: %s' % (sample[kn.sample_id_col],
                                                                                  self.validator.errors))
